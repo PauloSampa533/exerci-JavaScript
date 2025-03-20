76e64303203abb88e1e313648933ad6d34104c66
Paulo Ricardo Sampaio Lopese <paulosampaiocontato@gmail.comm>
2025-03-19 22:50:07 -0300

    exercicio 1

---
+++ 01-operacoesAritmetricas.js
@@ -0,0 +1,16 @@
+//EXERCICIO 1
+// Faça as 4 operações basicas com base nos números 4 e 4:
+//ao final de cada operação imprima na tela no seguinte formato
+//soma: 4 + 4 = x
+//Subtração: 4 - 4 = x
+//Divisão: 4 / 4 = x
+//Multiplicação: 4 * 4 = x
+//---------------------------------------------------------------------
+
+let num1 = 4;
+let num2 = 4;
+
+console.log('soma: 4 + 4 = ' + (num1 + num2));
+console.log('subtração: 4 - 4 = ' + (num1 - num2));
+console.log('divisão: 4 / 4 = ' + (num1 / num2));
+console.log('Multiplicação: 4 * 4 = ' + (num1 * num2));
