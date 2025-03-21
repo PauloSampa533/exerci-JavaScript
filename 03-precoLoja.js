7bcaed5188dbf4080ffc76bb80c1f44e52223381
Paulo Ricardo Sampaio Lopese <paulosampaiocontato@gmail.comm>
2025-03-21 15:23:14 -0300

    exercicio 03 preçoLoja

---
+++ 03-precoLoja.js
@@ -0,0 +1,15 @@
+
+let preco = 100;
+let aVista = 10;
+let totalAvista = 0;
+
+totalAvista = ( preco - (aVista / 100) * preco);
+
+console.log(`Preço: R$${preco},00`);
+console.log(`Á vista: R$${totalAvista},00`);
+console.log(`Ou 3x de: R$${preco/3},00`);
+
+
+
+
+
---
+++ 04-solveMeFirst-funcao.js
@@ -0,0 +1,8 @@
+function solveMeFirst(a, b) {
+    return a + b;
+  }
+  console.log(solveMeFirst(7, 3));
+
+ 
+
+ 
